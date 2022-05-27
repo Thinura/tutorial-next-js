1fa78879fdd69236b1368fba01a4357ce4fdcbe3
DESKTOP-NUQQ0IR\Thinura Kumarasinghe <thinura.kumarasinghe@sysco.com>
2022-05-28 00:45:11 +0530

    setup of meetup website

---
+++ pages/news/index.js
@@ -0,0 +1,18 @@
+import Link from "next/link"
+import { Fragment } from "react"
+
+function NewsPage() {
+    return (
+        <Fragment>
+            <h1>This is News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-great">NextJS Is A Great Framework</Link>
+                </li>
+                <li>Something Else</li>
+            </ul>
+        </Fragment>
+    )
+}
+
+export default NewsPage
